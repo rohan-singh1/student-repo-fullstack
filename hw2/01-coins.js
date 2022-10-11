22153e34cebd09e59f56ef7c1d55ff4925a6315f
Rohan Singh <vicky94rohan@gmail.com>
2022-10-11 00:50:06 -0700

    Coins program code beautified

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -1,38 +1,34 @@
 /** Exercise 01 - Coins **/
 
 const calculateChange = (input) => {
-  if (input > 10){
+  if (input > 10) {
     return "Oops, amount too heavy to handle! Please enter an amount no more than 10!\n";
-  }
-  else if (input < 0){
+  } else if (input < 0) {
     return "Negativity isn't allowed, neither in this program, nor in life. So please enter a positive value up to 10!\n";
-  }
-  else if (input >= 0 && input <= 10){
+  } else if (input >= 0 && input <= 10) {
     let final_message = "";
 
     let dollars = Math.trunc(input);
-    if (dollars > 0){
+    if (dollars > 0) {
       final_message += dollars;
-      if (dollars === 1){
+      if (dollars === 1) {
         final_message += " dollar, ";
-      }
-      else {
+      } else {
         final_message += " dollars, ";
       }
     }
 
     let decimal = input - dollars;
     decimal *= 100;
-    decimal = Math.round(decimal);  // Rounding any decimal digits after two places
+    decimal = Math.round(decimal); // Rounding any decimal digits after two places
 
     let quarters = decimal / 25;
     quarters = Math.trunc(quarters);
-    if (quarters > 0){
+    if (quarters > 0) {
       final_message += quarters;
-      if (quarters === 1){
+      if (quarters === 1) {
         final_message += " quarter, ";
-      }
-      else {
+      } else {
         final_message += " quarters, ";
       }
     }
@@ -40,12 +36,11 @@ const calculateChange = (input) => {
 
     let dimes = decimal / 10;
     dimes = Math.trunc(dimes);
-    if (dimes > 0){
+    if (dimes > 0) {
       final_message += dimes;
-      if (dimes === 1){
+      if (dimes === 1) {
         final_message += " dime, ";
-      }
-      else {
+      } else {
         final_message += " dimes, ";
       }
     }
@@ -53,31 +48,28 @@ const calculateChange = (input) => {
 
     let nickels = decimal / 5;
     nickels = Math.trunc(nickels);
-    if (nickels > 0){
+    if (nickels > 0) {
       final_message += nickels;
-      if (nickels === 1){
+      if (nickels === 1) {
         final_message += " nickel, ";
-      }
-      else {
+      } else {
         final_message += " nickels, ";
       }
     }
     decimal = decimal % 5;
 
     let pennies = decimal;
-    if (pennies > 0){
-      if (pennies === 1){
+    if (pennies > 0) {
+      if (pennies === 1) {
         final_message += pennies + " penny";
-      }
-      else {
+      } else {
         final_message += pennies + " pennies";
       }
     }
 
     final_message += "\n";
     return final_message;
-  }
-  else {
+  } else {
     return "Nice try QA engineer! Only numeric values between 0 and 10 allowed!\n";
   }
 };
@@ -93,7 +85,7 @@ console.log(calculateChange(15.11));
 // $15.11 ==> Oops, amount too heavy to handle! Please enter an amount no more than 10!
 console.log(calculateChange("ABCD"));
 // ABCD ==> Nice try QA engineer! Only numeric values between 0 and 10 allowed!
-console.log(calculateChange(-4.50));
+console.log(calculateChange(-4.5));
 // Negativity isn't allowed, neither in this program, nor in life. So please enter a positive value up to 10!
 console.log(calculateChange(3.45678));
 // 2.345678 ==> 2 dollars, 1 quarter, 2 dimes, 1 penny
